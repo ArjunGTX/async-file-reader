bc4d4088afccb8e2d099df8fc3ecfa1735ebbb77
ArjunGTX <arjunatpavilion@gmail.com>
2022-07-27 23:18:22 +0530

    added word count

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs;
 use std::thread;
 fn main() {
+    // async_file();
+    word_count();
+}
+
+fn async_file() {
     let handle1 = thread::spawn(|| {
         println!("File one started");
         let file = fs::read_to_string("./src/test1.txt").unwrap();
@@ -13,4 +18,13 @@ fn main() {
     });
     handle1.join().unwrap();
     handle2.join().unwrap();
+}
+
+fn word_count() {
+    let file = fs::read_to_string("./src/test1.txt").unwrap();
+    let mut count = 0;
+    for _word in file.split_whitespace() {
+        count += 1;
+    }
+    println!("{}", count)
 }
